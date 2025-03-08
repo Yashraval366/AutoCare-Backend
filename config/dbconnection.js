52e666f4db7d524ef17f83bba1e90635e7d2d11d
Yash raval <123889939+Yashraval366@users.noreply.github.com>
2025-03-08 09:20:47 +0530

    commit env

--- config/dbconnection.js
+++ config/dbconnection.js
@@ -1,10 +1,10 @@
 const mysql2 = require('mysql2')
 
 let mysqlConnection = mysql2.createPool({
-    host: "localhost",
-    user: "root",
-    password: "yash7575@720",
-    database: "newprojectdb",
+    host: process.env.DB_HOST_NAME,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
     waitForConnections: true,
     connectionLimit: 10, 
     queueLimit: 0
