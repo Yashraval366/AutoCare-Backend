897df4200caf357a6de7e94e5fb6b91e0f99abb3
Vaishnavi4924 <vaishnavipadhiyar13@gmail.com>
2025-03-12 21:57:03 +0530

    booklsot

--- Routes/bookslotRoute.js
+++ Routes/bookslotRoute.js
@@ -1,7 +1,9 @@
+const getBookslotData = require('../controllers/bookslotget')
 const express = require('express')
 const setBookslotData = require('../controllers/bookslotController')
 const router = express.Router()
 
 router.route("/setbookslot").post(setBookslotData)
+router.route("/getbookslot").get(getBookslotData)
 
 module.exports = router;
